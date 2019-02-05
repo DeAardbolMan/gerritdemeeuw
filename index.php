fd2f5fd5ab824d7c443a5bf0c027d9cfee977243
DeAardbolMan <43012105+DeAardbolMan@users.noreply.github.com>
2019-02-05 09:34:12 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+  <title>Gerritdemeeuw.nl [*] Het verhaal van Gerrit</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Hoi, ik ben Gerrit en ik ben een duif... althans dat zeggen ze. Ik voel mij eigenlijk een meeuw, elke keer dat ik dat vertel, word ik voor gek verklaard.">
+    <meta name="keywords" content="Gerrit, Gerrit de meeuw, Gerritdemeeuw, gerritdemeeuw.nl, #gerritdemeeuw, #kijkeenmeeuw meeuw, kijkeenmeeuw, gerritdemeeuw.nl, meeuw gerrit, vogel, voggel, bami, jari jongenjan, gerrit, groningen, rotterdam, zuidplein, goeree, haringvliet">
+    <meta name="author" content="Borre">
+	<link rel="stylesheet" href="style.css">
+
+	<link rel="canonical" href="https://gerritdemeeuw.nl/index.php" />
+	
+
+
+</head>
+<body>
+	<header>
+	<article>
+		<h1>Gerrit de meeuw</h1>
+		<p>#gerritdemeeuw</p>
+		<div class="filmpjegerrit">
+		</div>
+		</article>
+		
+	</header>
+	<main>
+		<section>
+		<article>
+		<header>
+			<h2>Even voorstellen</h2>
+		</header>
+			<p>Hoi, ik ben Gerrit en ik ben een duif... althans dat zeggen ze. Ik voel mij eigenlijk een meeuw, elke keer dat ik dat vertel, word ik voor gek verklaard.
+			Ja.. het is misschien een beetje raar, een meeuw die eigenlijk een duif is, maar zo is het leven nou eenmaal tegenwoordig. Welkom in <?php echo date("Y"); ?>.</p>
+			
+			<p>Samen met mijn andere duiven-vrienden spendeer ik het grootste gedeelte van mijn tijd in Rotterdam, in de buurt van winkelcentrum Zuidplein. 
+			Wisten jullie dat dat het grootste winkelcentrum van Nederland is? Door die grote ruimte is het ge-wel-dig om daar rond te vliegen.</p>
+			
+			
+			<!--script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-8917422504351930"
+     data-ad-slot="4242773201"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script-->
+
+		</article>
+		</section>
+		<section class="gerrit" id="gerrit">
+		<article>
+		<header>
+			<h2>Verhaaltjestijd</h2>
+		</header>
+			<p>Wow, geweldig! Je wilt dit onzinnige verhaal gewoon gaan lezen... heb je niks beters te doen met je leven? Hmm.. nee blijkbaar niet want je leest nog steeds door.
+			Nou, zullen we maar eens beginnen met het verhaal?</p>
+			<p><b>Er was eens..</b> een meeuw, of eigenlijk een duif met als naam Gerrit [wow, dit verhaal gaat over mij! :) ]. 
+			Ik ben dus Gerrit en zoals je inmiddels al weet ben ik een duif, maar ik zou graag een meeuw geweest zijn.
+			Het is nou eenmaal veel leuker om patatjes te pikken van niksvermoedende aardbewoners dan post te bezorgen voor jullie stomme zaken. 
+			Ben ik blij dat ik daar als meeuw geen last van heb.</p>
+			<p>Toen ik uit het ei dat mijn moeder gelegd heeft gekropen was, zag ik deze geweldige wereld waar ik nu nog steeds op leef. 
+			In alle jaren is er wel een hoop veranderd in Rotterdam, zo zijn er heel wat aardbewoners bij gekomen.. weet je dat ik een keer bijna aangereden ben door een metro? 
+			Ik kan je vertellen dat dat echt géén pretje was, een bijna dood ervaring. Gelukkig kon ik net op tijd wegvliegen, anders was er nu misschien geen Gerrit de meeuw pagina.. ;( </p>
+		</article>
+		</section>
+		<section>
+		<article>
+		<header>
+			<h2>Een wereldreis</h2>
+		</header>
+			<p>Heel leuk, dat leven in Rotterdam, maar een meeuw, die moet de wereld zien. Dus ik ging op jonge leeftijd al op pad met als doel om zo veel mogelijk van deze ge-wel-dig-ge aardkloot te ontdekken. 
+			Natuurlijk zijn die gekke aardbewoners al bijna overal geweest, maar het is toch veel mooier om zelf alles gezien te hebben?! Het begon allemaal in Rotterdam, nadat ik groot genoeg was 
+			om te vliegen ging ik op pad. Mijn eerste bestemming was Groningen, mooi om te zien hoe ver dat volk nog achter loopt. Van Groningen ging ik naar Antwerpen, een prachtige stad, maar dat volk wat daar woont is volgens mij
+			niet het slimste volk wat er bestaat. Er liepen twee kinderen over straat met een bak frietjes, ja zo noemen ze die dingen daar, en toen liet een van die kinderen heel dat bakje op straat donderen.. hmm, geluk voor mij. 
+			Ik vloog zo snel als ik kon naar de heerlijke Vlaamse frietjes en begon met eten en ik werd gewoon met rust gelaten door die kinderen.</p>
+			<!--script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-8917422504351930"
+     data-ad-slot="5188899656"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script-->
+			<p>Terwijl ik zat te eten kwam er ineens een irritant snertjong aanlopen die mijn dag wilde verpesten. Daar had ik natuurlijk geen zin in, dus ik ben met mijn bakkes vol friet doorgevlogen naar mijn volgende bestemming. Parijs.
+			Eenmaal aangekomen in Parijs werd ik verrast door een Fransman met een stukje "baguette", stokbrood in het Frans. <a href="#wereldreis">Lees verder.</a></p>
+			</article>
+		</section>
+		<section class="gerrit" id="gerrit">
+		<article>
+		<header>
+		<h2>Een kijkje in mijn leven</h2>
+		</header>
+			<div class="fotogerrit">
+		</div>
+		<p>Hmm, kijk dat ben ik! Wat een prachtige meeuw ben ik toch. Zoals je ziet ben ik niet zo'n fan van camera's, het is dan ook uniek dat ik er hier zo goed uit zie op de camera.
+		Dit filmpje is gemaakt op het winkelcentrum in Rotterdam, op Zuidplein dus. Ik was hier heerlijk een stukje aan het lopen, totdat er ineens een camera op mijn gezicht gericht werd, 
+		verschrikkelijk al die telefoons tegenwoordig. Er gaat geen dag meer voorbij zonder telefoon. Wist je dat ik nog nooit van mijn leven een telefoon in mijn handen heb gehad? 
+		Ik heb trouwens helemaal geen handen...</p>
+		</article>
+		</section>
+		<section id="wereldreis">
+		<article>
+		<header>
+			<h2>Een wereldreis: het vervolg</h2>
+		</header>
+			<p><img src="spanje.png" alt="Gerrit in Spanje.png" style="float: right;">Hmm, wat een lekker stukje stokbrood was dat! En ja.. aangezien ik van oorsprong een Nederlandse vogel ben.. gratis is altijd goed, toch? 
+			Wij staan als Nederlanders nou eenmaal bekend als een gierig volk. Genoeg gezeur over Nederlanders, we gaan weer verder met het verhaal. 
+			Wat fijn trouwens dat je de tijd hebt genomen om dit verhaal te lezen, 
+			volg je <a href="https://instagram.com/kijkeenmeeuw/">@kijkeenmeeuw</a> al op Instagram? Schaamteloze zelfpromotie, dat mag gewoon toch? Maar we hadden beloofd om verder 
+			te gaan met het verhaal en je weet wat ze zeggen... belofte maakt schuld. Dus daar was ik, in mijn eentje in Parijs met een stukje stokbrood in mijn bek. Na het heerlijke stokbrood
+			opgegeten te hebben ben ik mijn reis vervolgd, ik heb tussendoor nog een tussenstop gemaakt bij de Eiffeltoren en bij het Louvre, een museum in Parijs voor als je dat nog niet wist. 
+			Na een stuk vliegen kwam ik aan in Spanje, in Barcelona om precies te zijn. Daar heb ik heel even gekeken hoe FC Barcelona aan het voetballen was en ik ben weer doorgevlogen. </p>
+			
+			<p>Nadat ik in Barcelona was geweest ben ik doorgevlogen naar Londen, in Londen ben ik onder de London Bridge door gevlogen en vervolgens een paar rondjes rondom de Big Ben.
+			Ik was vanplan om een rondje mee te gaan met de London Eye, een enorm reuzenrad, maar ik mocht helaas niet naar binnen. Vervolgens heb ik even een tijdje rust genomen voordat 
+			ik begon aan mijn terugreis naar Rotterdam. 
+			</p>
+			
+			</article>
+		</section>
+		
+		
+	</main>
+	<footer>
+	<article>
+			<p>
+			<a href="https://www.instagram.com/explore/tags/kijkeenmeeuw/" target="_blank">#kijkeenmeeuw</a><br>
+			<a href="https://www.instagram.com/kijkeenmeeuw/" target="_blank">@kijkeenmeeuw</a><br>
+			<a href="https://www.instagram.com/explore/tags/gerritdemeeuw/" target="_blank">#gerritdemeeuw</a><br>
+			</p>
+			<p>&copy; Copyright <a href="https://kijkeenmeeuw.nl/"><b>kijkeenmeeuw.nl</b></a> // <b>gerritdemeeuw.nl</b></p>
+			<p>Designed / coded by <a href="https://instagram.com/borrevde/"><b>Borre</b></a></p>
+			
+			</article>
+	</footer>
